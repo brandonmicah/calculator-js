7ace3346e0cab9a633ffb3d8f37f1747048c2972
Brandon O'Regan <brandon_oregan97@hotmail.com>
2023-05-12 15:42:16 +0100

    basic html/css

---
+++ script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+// function when a click event happens the buttons value is added to an equation area
+// that adds the equation area to the display area
+// function that clears display and equation area
