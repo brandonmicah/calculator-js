1c597d57d026fdda311eb4c0d496d2524aa2d7e2
Brandon O'Regan <brandon_oregan97@hotmail.com>
2023-05-15 15:53:24 +0100

    Refactored

--- script.js
+++ script.js
@@ -6,7 +6,9 @@
 
 const gridBtns = document.querySelectorAll(".grid--btns");
 const display = document.querySelector(".display--input");
-
+const clearBtn = document.querySelector(".clear--btn");
+const equalsBtn = document.getElementById("equals");
+const calcFace = document.querySelector(".face--container");
 let calcArr = [];
 // let clickedValue;
 
@@ -24,21 +26,64 @@ const pushCalcSpace = function (e) {
   console.log(calcArr);
 };
 
-gridBtns.forEach((button) => {
-  button.addEventListener("click", function (e) {
-    if (
-      e.target.dataset.button === "plus" ||
-      e.target.dataset.button === "minus" ||
-      e.target.dataset.button === "multiply" ||
-      e.target.dataset.button === "divide"
-    ) {
-      pushCalcSpace(e);
-    } else if (e.target.dataset.button === "equals") {
-      console.log(calculate);
-    } else {
-      pushCalc(e);
-    }
-  });
+const equal = function (arr) {
+  const newArr = arr.filter((el) => el !== " ").join("");
+  calcArr = [];
+  display.innerHTML = eval(newArr);
+};
+
+// gridBtns.forEach((button) => {
+//   button.addEventListener("click", function (e) {
+//     if (
+//       e.target.dataset.button === "plus" ||
+//       e.target.dataset.button === "minus" ||
+//       e.target.dataset.button === "multiply" ||
+//       e.target.dataset.button === "divide"
+//     ) {
+//       pushCalcSpace(e);
+//     } else if (e.target.dataset.button === "equals") {
+//       console.log(calculate);
+//     } else {
+//       pushCalc(e);
+//     }
+//   });
+// });
+
+calcFace.addEventListener("click", function (e) {
+  if (e.target === equalsBtn) equal(calcArr);
+  if (e.target === clearBtn) {
+    display.innerHTML = "";
+    calcArr = [];
+  }
+  if (
+    e.target.dataset.button === "plus" ||
+    e.target.dataset.button === "minus" ||
+    e.target.dataset.button === "multiply" ||
+    e.target.dataset.button === "divide"
+  )
+    pushCalcSpace(e);
+  if (
+    e.target.dataset.button === "zero" ||
+    e.target.dataset.button === "one" ||
+    e.target.dataset.button === "two" ||
+    e.target.dataset.button === "three" ||
+    e.target.dataset.button === "four" ||
+    e.target.dataset.button === "five" ||
+    e.target.dataset.button === "six" ||
+    e.target.dataset.button === "seven" ||
+    e.target.dataset.button === "eight" ||
+    e.target.dataset.button === "nine"
+  )
+    pushCalc(e);
+
+  //   console.log(e.target)
 });
 
 // console.log(calculate);
+
+// clearBtn.addEventListener("click", function () {
+//   display.innerHTML = "";
+//   calcArr = [];
+// });
+
+// equalsBtn.addEventListener("click", function () {});
